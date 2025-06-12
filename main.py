93e54eec38d8070e00cb61e58843931029c1f874
Oleksandra Shevchenko <109725772+Oleksandra-Shevchenko@users.noreply.github.com>
2025-06-12 17:10:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ import pandas as pd
 import time
 from tqdm import tqdm
 
-# 📥 Разрешено 1 раз в минуту
-print("Запрашиваем список всех игр (1 раз)...")
 time.sleep(60)  # safety delay to avoid violating the rate limit
 all_url = "https://steamspy.com/api.php?request=all"
 app_data = requests.get(all_url).json()
 app_ids = list(app_data.keys())
-print(f"Получено {len(app_ids)} appid")
 
-# Подготовка к циклу запросов
 all_games = []
-DELAY_SECONDS = 1  # между appdetails-запросами
+DELAY_SECONDS = 1  
 MAX_RETRIES = 3
 
-print(f"Собираем подробную информацию по {len(app_ids)} играм...")
 
 for appid in tqdm(app_ids):
     for attempt in range(MAX_RETRIES):
@@ -28,11 +23,9 @@ for appid in tqdm(app_ids):
             all_games.append(game)
             break
         except Exception as e:
-            print(f"⚠️ Ошибка {appid}, попытка {attempt+1}: {e}")
+            print(f"⚠️ erroe {appid}: {e}")
             time.sleep(2)
     time.sleep(DELAY_SECONDS)
 
-# Сохраняем в CSV
 df = pd.DataFrame(all_games)
 df.to_csv("steamspy_full_dataset.csv", index=False, encoding="utf-8-sig")
-print("✅ Данные сохранены в steamspy_full_dataset.csv")
